3fb5e3e989f43c8824f529fa0dac8e74e646a703
Kris Jenkins <kjenkins@confluent.io>
2021-12-10 09:31:57 +0000

    Year 2021, Day 1.

---
+++ src/Year2021/Day1.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module Year2021.Day1
+  ( solution1,
+    solution2,
+  )
+where
+
+import Text.Megaparsec (sepEndBy)
+import Text.Megaparsec.Char (eol)
+import Utils (integer, simpleParse)
+
+readSequence :: IO [Int]
+readSequence = simpleParse "data/Year2021/Day1.txt" (integer `sepEndBy` eol)
+
+solution1 :: IO Int
+solution1 = do
+  depths <- readSequence
+  pure $ length $ filter id $ zipWith (<) depths (tail depths)
+
+solution2 :: IO Int
+solution2 = do
+  depths <- readSequence
+  let windows :: [Int]
+      windows = zipWith3 (\a b c -> a + b + c) depths (tail depths) (tail (tail depths))
+  pure $ length $ filter id $ zipWith (<) windows (tail windows)
