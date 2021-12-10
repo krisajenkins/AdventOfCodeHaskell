d3f4bfff9c1d05922996d1262f64f9ba7e6dd833
Kris Jenkins <kjenkins@confluent.io>
2021-12-10 09:31:22 +0000

    Updating the nix build and pinning nixpkgs with niv.

--- shell.nix
+++ shell.nix
@@ -1,8 +1,16 @@
-{ pkgs ? import <nixpkgs> { } }:
-pkgs.mkShell {
+{ sources ? import ./nix/sources.nix }:
+let
+  pkgs =
+    import sources.nixpkgs {
+      overlays = [ ];
+    };
+in
+pkgs.mkShell
+{
   buildInputs = [
     pkgs.stack
     pkgs.cabal-install
+    pkgs.haskell-language-server
     pkgs.libiconv
   ];
 }
